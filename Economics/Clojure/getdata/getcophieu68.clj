4e7558984c7bf76704d354131a4885913965ad71
Thinh P. Tran <trphthinh@gmail.com>
2021-04-23 11:16:08 +0700

    parsing web data

--- Economics/Clojure/getdata/getcophieu68.clj
+++ Economics/Clojure/getdata/getcophieu68.clj
@@ -14,7 +14,7 @@
 (ns icy-meadow
   (:require [gorilla-plot.core :as plot]
             [etaoin.api :as eapi]
-            [etaoin.keys :as k])
+            [etaoin.keys :as k]))
 ;; @@
 ;; =>
 ;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
@@ -26,18 +26,52 @@
 
 ;; @@
 (def driver (eapi/firefox))
-;; @@
-;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;icy-meadow/driver</span>","value":"#'icy-meadow/driver"}
-;; <=
-
-;; @@
 (eapi/go driver "https://en.wikipedia.org/")
 ;; @@
 ;; =>
 ;;; {"type":"list-like","open":"<span class='clj-map'>{</span>","close":"<span class='clj-map'>}</span>","separator":", ","items":[{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:value</span>","value":":value"},{"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}],"value":"[:value nil]"}],"value":"{:value nil}"}
 ;; <=
 
-;; @@
+;; **
+;;; Get data from Hoa Phat
+;; **
 
+;; @@
+(def driver (eapi/firefox))
+(eapi/go driver "https://www.cophieu68.vn/historyprice.php?id=hpg")
+(eapi/wait-visible driver {:id :advancedButton})
+(eapi/click driver {:id :advancedButton})
+(eapi/wait-visible driver {:id :exceptionDialogButton})
+(eapi/click driver {:id :exceptionDialogButton})
+;; @@
+;; =>
+;;; {"type":"list-like","open":"<span class='clj-map'>{</span>","close":"<span class='clj-map'>}</span>","separator":", ","items":[{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:value</span>","value":":value"},{"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}],"value":"[:value nil]"}],"value":"{:value nil}"}
+;; <=
+
+;; **
+;;; Get headers
+;; **
+
+;; @@
+(->> (eapi/query-all driver [{:tag :table :fn/has-class "stock"}{:tag :tbody}{:tag :tr :fn/has-class "tr_header"}{:tag :td}])
+     (map #(eapi/get-element-inner-html-el driver %)))
+;; @@
+;; =>
+;;; {"type":"list-like","open":"<span class='clj-lazy-seq'>(</span>","close":"<span class='clj-lazy-seq'>)</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-string'>&quot;STT&quot;</span>","value":"\"STT\""},{"type":"html","content":"<span class='clj-string'>&quot;Ngày&quot;</span>","value":"\"Ngày\""},{"type":"html","content":"<span class='clj-string'>&quot;Giá Tham Chiếu&quot;</span>","value":"\"Giá Tham Chiếu\""},{"type":"html","content":"<span class='clj-string'>&quot;+/- (*)&quot;</span>","value":"\"+/- (*)\""},{"type":"html","content":"<span class='clj-string'>&quot; % (*)&quot;</span>","value":"\" % (*)\""},{"type":"html","content":"<span class='clj-string'>&quot;Đóng Cửa (*)&quot;</span>","value":"\"Đóng Cửa (*)\""},{"type":"html","content":"<span class='clj-string'>&quot;Khối Lượng&quot;</span>","value":"\"Khối Lượng\""},{"type":"html","content":"<span class='clj-string'>&quot;Mở Cửa (*)&quot;</span>","value":"\"Mở Cửa (*)\""},{"type":"html","content":"<span class='clj-string'>&quot;Cao Nhất(*)&quot;</span>","value":"\"Cao Nhất(*)\""},{"type":"html","content":"<span class='clj-string'>&quot;Thấp Nhất(*)&quot;</span>","value":"\"Thấp Nhất(*)\""},{"type":"html","content":"<span class='clj-string'>&quot;Giao Dịch Thỏa Thuận&quot;</span>","value":"\"Giao Dịch Thỏa Thuận\""},{"type":"html","content":"<span class='clj-string'>&quot;Nước Ngoài Mua&quot;</span>","value":"\"Nước Ngoài Mua\""},{"type":"html","content":"<span class='clj-string'>&quot;Nước Ngoài Bán&quot;</span>","value":"\"Nước Ngoài Bán\""}],"value":"(\"STT\" \"Ngày\" \"Giá Tham Chiếu\" \"+/- (*)\" \" % (*)\" \"Đóng Cửa (*)\" \"Khối Lượng\" \"Mở Cửa (*)\" \"Cao Nhất(*)\" \"Thấp Nhất(*)\" \"Giao Dịch Thỏa Thuận\" \"Nước Ngoài Mua\" \"Nước Ngoài Bán\")"}
+;; <=
+
+;; @@
+(->> (eapi/query-all driver [{:tag :table :fn/has-class "stock"}{:tag :tbody}{:tag :tr :fn/has-class "tr_header"}{:tag :td}])
+     (map #(eapi/get-element-attr-el driver % "title")))
+;; @@
+;; =>
+;;; {"type":"list-like","open":"<span class='clj-lazy-seq'>(</span>","close":"<span class='clj-lazy-seq'>)</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-string'>&quot;So thu tu&quot;</span>","value":"\"So thu tu\""},{"type":"html","content":"<span class='clj-string'>&quot;Ngày&quot;</span>","value":"\"Ngày\""},{"type":"html","content":"<span class='clj-string'>&quot;Giá Tham Chiếu&quot;</span>","value":"\"Giá Tham Chiếu\""},{"type":"html","content":"<span class='clj-string'>&quot;Thay Đổi&quot;</span>","value":"\"Thay Đổi\""},{"type":"html","content":"<span class='clj-string'>&quot;Thay Đổi %&quot;</span>","value":"\"Thay Đổi %\""},{"type":"html","content":"<span class='clj-string'>&quot;Đóng Cửa&quot;</span>","value":"\"Đóng Cửa\""},{"type":"html","content":"<span class='clj-string'>&quot;Khối Lượng&quot;</span>","value":"\"Khối Lượng\""},{"type":"html","content":"<span class='clj-string'>&quot;&quot;</span>","value":"\"\""},{"type":"html","content":"<span class='clj-string'>&quot;&quot;</span>","value":"\"\""},{"type":"html","content":"<span class='clj-string'>&quot;&quot;</span>","value":"\"\""},{"type":"html","content":"<span class='clj-string'>&quot;Giao Dịch Thỏa Thuận&quot;</span>","value":"\"Giao Dịch Thỏa Thuận\""},{"type":"html","content":"<span class='clj-string'>&quot;Nước Ngoài Mua&quot;</span>","value":"\"Nước Ngoài Mua\""},{"type":"html","content":"<span class='clj-string'>&quot;Nước Ngoài Bán&quot;</span>","value":"\"Nước Ngoài Bán\""}],"value":"(\"So thu tu\" \"Ngày\" \"Giá Tham Chiếu\" \"Thay Đổi\" \"Thay Đổi %\" \"Đóng Cửa\" \"Khối Lượng\" \"\" \"\" \"\" \"Giao Dịch Thỏa Thuận\" \"Nước Ngoài Mua\" \"Nước Ngoài Bán\")"}
+;; <=
+
+;; **
+;;; Get rows
+;; **
+
+;; @@
+(->> (eapi/query-all driver))
 ;; @@
--- Economics/Clojure/getdata/project.clj
+++ Economics/Clojure/getdata/project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [etaoin "0.3.6"]]
+                 [etaoin "0.4.1"]
+                 [hickory "0.6.0"]]
   :plugins [[org.clojars.benfb/lein-gorilla "0.7.0"]]
   :repl-options {:init-ns getdata.core})
